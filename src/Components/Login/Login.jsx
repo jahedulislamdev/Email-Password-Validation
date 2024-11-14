d4a167ad8b23466d3eb2d8daeddde9be4d9d9f83
JISHAN <jahedulislamdev@gmail.com>
2024-11-14 23:25:52 +0600

    add chaeckbox and it's conditions

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -11,9 +11,10 @@ const Login = () => {
       e.preventDefault();
       const email = e.target.email.value;
       const password = e.target.password.value;
-      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      const acceptTerms = e.target.terms.checked
+      // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
       setPassError('');
-      //Check password with dffrent condition and error messages.
+      //Check password with different condition and error messages.
       if (!/[A-Z]/.test(password)) {
          setPassError("your password should have at least one uppercase charecter")
          return;
@@ -29,6 +30,9 @@ const Login = () => {
       }
       else if (password.length < 8) {
          setPassError("password should be 8 charecter or longer ")
+         return;
+      } else if (!acceptTerms) {
+         setPassError("Please accept our terms and conditions")
          return;
       }
       //Check one time for strong password
@@ -69,13 +73,16 @@ const Login = () => {
                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                            </svg>
-
                         }</button>
                      </div>
-                     {passError && <p className='text-xs text-red-500'>{passError}</p>}
+                     {passError && <p className='text-xs text-red-500  p-1'>{passError}</p>}
                      <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                      </label>
+                  </div>
+                  <div className='flex'>
+                     <input type="checkbox" name='terms' id='terms' />
+                     <label className='ms-1' htmlFor="terms">Accept our <a href="#">Terms and conditions</a></label>
                   </div>
                   <div className="form-control mt-6">
                      <button className="btn btn-primary">Login</button>
