9f48751c0113da166b04fa6bedceff02dda76388
JISHAN <jahedulislamdev@gmail.com>
2024-11-16 14:49:13 +0600

    add register page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -55,17 +55,17 @@ const Login = () => {
                <form className="card-body" onSubmit={handleForm}>
                   <div className="form-control">
                      <label className="label">
-                        <span className="label-text">Email</span>
+                        <span className="label-text">Email*</span>
                      </label>
                      <input type="email" placeholder="Your Email" name='email' className="input input-bordered" required />
                   </div>
                   <div className="form-control">
                      <label className="label">
-                        <span className="label-text">Password</span>
+                        <span className="label-text">Password*</span>
                      </label>
                      <div className='join'>
                         <input type={showPassword ? "text" : "password"} name='password' placeholder="password" className="input input-bordered join-item" required />
-                        <button onClick={() => setShowPassword(!showPassword)} className='bg-gray-600 p-1 join-item'>{showPassword ?
+                        <button onClick={() => setShowPassword(!showPassword)} className='bg-gray-800 p-1 join-item'>{showPassword ?
                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                               <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
